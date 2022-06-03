b699f7726846125ae023490c6f6c9f84bdfef078
akaur091 <amniderkaur.09@gmail.com>
2022-06-02 23:21:06 -0700

    add pseudo code for script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ var date = moment().format("MMMM Do YY");
 // display date (maybe if i works)
 $("#currentDay").append(date);
 
+// define hours 
+// create a function for updateing time using if else statements
+// create an on click event listner to save to local storage
+// add function to get each hour (child) and set it into the parent
